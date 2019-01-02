b1b588843a699452fdd5c87f06a958490990f386
ge0th <p16034@students.cs.unipi.gr>
2019-01-02 17:09:11 +0200

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,14 +1,27 @@
 import os
+import json
 import sys
 import math
 import pandas as pd
 import numpy as np
+from random import shuffle
 
 NUMBER_OF_USERS = 943
 GENRES = ['Action', 'Adventure', 'Animation',
           'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
           'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
           'Thriller', 'War', 'Western']
+
+
+def file_exists(file_name):
+
+
+    # return os.path.isfile(os.path.abspath(file_name))
+    return False
+
+def euclidian_distance(vector_a, vector_b):
+
+    return math.sqrt(sum([(vector_a - vector_b) ** 2 for vector_a, vector_b in zip(vector_a, vector_b)]))
 
 def binary_search(array, item):
 
@@ -19,7 +32,7 @@ def binary_search(array, item):
         mid = (first + last) // 2
 
         if array[mid] == item:
-            return mid # Found at mid
+            return True  # Found at mid
         elif array[mid] > item:
             last = mid - 1
         elif array[mid] < item:
@@ -28,40 +41,67 @@ def binary_search(array, item):
             return False
 
 
-def euclidian_distance(vector_a, vector_b):
+def vector_to_group(vector, groups):
+    """ vector_to_group finds the group index that a specific vector has been
+        placed
+    """
 
-    return math.sqrt(sum([(vector_a - vector_b) ** 2 for vector_a, vector_b in zip(vector_a, vector_b)]))
-
-def vector_to_group(vector_index, groups):
-    
     for index, group in enumerate(groups):
-        group_index = binary_search(group, vector_index)
+        group_index = binary_search(group, vector)
 
         if group_index == False:
             continue
+            print("roufa1")
         else:
-            return group_index
-        
+            print("roufa2")
+            print("index")
+            print(index)
+            return index
 
+def min_distance_vector(vector_index, vectors):
+    """ This function find the minimum distance between a specific vector and all the others
 
-    
-def min_distance_vector(vector, vectors):
+    """
 
-    min_target = 1000000000000
-    min_index = 1000000000000
+    min_distance = 10000000
+    min_index = vector_index
 
     for index, target in enumerate(vectors):
 
-        if euclidian_distance(vector, target) < min_target:
-            min_target = target
+        if euclidian_distance(vectors[vector_index], target) < min_distance:
+            min_distance = euclidian_distance(vectors[vector_index], target)
             min_index = index
+    print(vectors[min_index], min_distance)
 
-    return min_index, min_target
+    return vectors[min_index], min_distance
 
-    
 
-def max_vector(vectors):
-    pass
+def min_max_between_all(vectors):
+
+    distances = []
+
+    if file_exists('distances.json'):
+        with open('distances.json') as file:
+
+            for line in file:
+
+                line = line.strip()
+                distances.append(line)
+    else:
+
+        for i in range(0, len(vectors)):
+            for j in range(i + 1, len(vectors)):
+                distance = euclidian_distance(vectors[i], vectors[j])
+                distances.append(distance)
+
+        with open('distances.json', 'w') as file:
+            json.dump(distances, file)
+
+
+    return min(distances), max(distances)
+
+
+
 
 
 def BSAS(threshold, q, vectors):
@@ -71,9 +111,9 @@ def BSAS(threshold, q, vectors):
 
     vectors_count = len(vectors)
 
-    for i in range(2, vectors_count + 1):
-        
-        min_index, min_distance = min_distance_vector(vectors[i], vectors)
+    for i in range(1, vectors_count):
+        print("BSAS IIIII : {}".format(i))
+        min_vector, min_distance = min_distance_vector(i, vectors)
 
 
         if min_distance > threshold and m < q:
@@ -86,7 +126,12 @@ def BSAS(threshold, q, vectors):
             # Adds the new group to Cm list of groups
             Cm.append(new_group)
         else:
+            min_index = vector_to_group(min_vector, Cm)
+            print(min_index)
             Cm[min_index].append(vectors[i])
+    
+    return len(Cm)
+
 
 def cleaning_data():
     """This method prepares the data for processing by BSAS.
@@ -123,7 +168,7 @@ def cleaning_data():
                          encoding='latin-1')
 
 
-    print(movies.query('unknown == 1').head(10))
+
 
     movies = movies[movies.unknown == 0]
 
@@ -133,31 +178,60 @@ def cleaning_data():
 
     # Merging Ratings and Movies
 
-    final_matrix = pd.merge(ratings, movies, on='movie id', how='inner')
-    print(final_matrix.sort_values(by=['user id']).head(100))
+    final_matrix = pd.merge(ratings, movies, on='movie_id', how='inner')
         
-    df=final_matrix.replace(0, np.NaN) #all 0 values transform to NaN
+    df = final_matrix.replace(0, np.NaN) #all 0 values transform to NaN
 
-    df.loc[(df['Action']==1),'Action']=df['rating'] #if Action is the genre (value 1) then becomes the value of rating etc...
-    df.loc[(df['Adventure']==1),'Adventure']=df['rating']
-    df.loc[(df['Animation']==1),'Animation']=df['rating']
-    df.loc[(df['Children\'s']==1),'Children\'s']=df['rating']
-    df.loc[(df['Comedy']==1),'Comedy']=df['rating'] 
-    df.loc[(df['Crime']==1),'Crime']=df['rating']
-    df.loc[(df['Documentary']==1),'Documentary']=df['rating']
-    df.loc[(df['Drama']==1),'Drama']=df['rating']
-    df.loc[(df['Fantasy']==1),'Fantasy']=df['rating']
-    df.loc[(df['Film-Noir']==1),'Film-Noir']=df['rating']
-    df.loc[(df['Horror']==1),'Horror']=df['rating']
-    df.loc[(df['Musical']==1),'Musical']=df['rating']
-    df.loc[(df['Mystery']==1),'Mystery']=df['rating']
-    df.loc[(df['Romance']==1),'Romance']=df['rating']
-    df.loc[(df['Sci-Fi']==1),'Sci-Fi']=df['rating']
-    df.loc[(df['Thriller']==1),'Thriller']=df['rating']
-    df.loc[(df['War']==1),'War']=df['rating']
-    df.loc[(df['Western']==1),'Western']=df['rating']
+    for genre in GENRES:
+        df.loc[(df[genre] == 1), genre] = df['rating']
+
     
-    grouped=df.groupby('user id').mean() #group by user id and calculate the avg of non-NaN values
+    grouped=df.groupby('user_id').mean() #group by user id and calculate the avg of non-NaN values
     
-    grouped.drop(columns=['movie id','rating'], inplace=True, axis=1) #leave only user id and agv for every genre
-    print(grouped)
+    grouped.drop(columns=['movie_id','rating'], inplace=True, axis=1) #leave only user id and agv for every genre
+
+    grouped = grouped.replace(0, np.NaN)
+    grouped = list(grouped.values.tolist())
+
+    # Replaces nan with 0
+    for group in grouped:
+        for number in range(len(group)):
+            if math.isnan(group[number]):
+                group[number] = 0
+
+    with open('data.json', 'w') as file:
+        json.dump(grouped, file)
+
+    return grouped
+
+def get_clusters_count(a, b, c, s, vectors, q):
+
+    groups = []
+    for theta in range(a, b + 1, c):
+        for i in range(0, s):
+            shuffle(vectors)
+            count = BSAS(theta, q, vectors)
+            groups.append(count)
+    #         print("Found {} groups".format(count))
+    # print(groups)
+    return groups
+
+if __name__ == '__main__':
+    
+    if file_exists('data.json'):
+        vectors = []
+        with open('data.json') as file:
+            for line in file: 
+                line = line.strip() 
+                vectors.append(line) 
+    else:
+        vectors = cleaning_data()
+
+
+    a, b = min_max_between_all(vectors)[0], min_max_between_all(vectors)[1]
+    a, b = int(float(a)), int(float(b))
+
+    clusters = get_clusters_count(a, b, 400, 15, vectors, 400)
+    print(clusters)
+
+a = input()
