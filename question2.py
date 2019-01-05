0c696f882ca3853485e07f8bf262551762e883c3
ge0th <p16034@students.cs.unipi.gr>
2019-01-05 23:03:24 +0200

    Update question2.py

--- question2.py
+++ question2.py
@@ -17,6 +17,69 @@ GENRES = ['Action', 'Adventure', 'Animation',
           'Thriller', 'War', 'Western']
 
 
+def cleaning_data():
+    """This method prepares the data for processing by BSAS.
+
+        Especially it drops the unnecessary columns, removes the rows of movies 
+        with unknown genre and computes the average rating of each user of each
+        movie genre.
+    """
+
+    # Getting absolute path to the data files
+
+    udata = os.path.abspath('dataset/u.data')
+    uitem = os.path.abspath('dataset/u.item')
+
+    # Creating User Ratings Matrix
+
+    data_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+
+    ratings = pd.read_csv(udata, sep='\t', names=data_cols,
+                          encoding='latin-1')
+
+    ratings.drop(columns='timestamp', inplace=True, axis=1)
+
+    # Creating Movies Matrix
+
+    movie_cols = ['movie_id', 'movie_title', 'release_date', 'video_release date',
+                  'IMDb_URL', 'unknown', 'Action', 'Adventure', 'Animation',
+                  'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+                  'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
+                  'Thriller', 'War', 'Western']
+
+    movies = pd.read_csv(uitem, sep='|', names=movie_cols,
+                         encoding='latin-1')
+
+
+    movies = movies[movies.unknown == 0]
+
+    movies.drop(columns=['video_release date', 'release_date',
+                         'IMDb_URL', 'unknown', 'movie_title'], inplace=True, axis=1)
+
+    # Merging Ratings and Movies
+
+    final_matrix = pd.merge(ratings, movies, on='movie_id', how='inner')
+
+    df = final_matrix.replace(0, np.NaN)  # all 0 values transform to NaN
+
+    for genre in GENRES:
+        df.loc[(df[genre] == 1), genre] = df['rating']
+
+
+    grouped = df.groupby('user_id').mean() #group by user id and calculate the avg of non-NaN values
+
+    grouped.drop(columns=['movie_id', 'rating'], inplace=True, axis=1) #leave only user id and agv for every genre
+
+    grouped.fillna(0, inplace=True)
+    
+    grouped = list(grouped.values.tolist())
+
+    with open('data.json', 'w') as file:
+        json.dump(grouped, file)
+
+    return grouped
+
+
 def file_exists(file_name):
     """Checks if the specified file exists"""
 
@@ -64,18 +127,14 @@ def vector_to_group(vector, groups):
     for index, group in enumerate(groups):
         group_index = binary_search(group, vector)
 
-
-        #If vector is not found in the first group check the next
+        # If vector is not found in the first group check the next
         if group_index == False:
             continue
         else:
-            print("roufa2")
-            print("index")
-            print(index)
             return index
 
 
-def min_distance_vector(vector_index, vectors):
+def group_with_min_distance(vector, groups):
     """
     Returns the minimum euclidian distance between the giver vector and the remainings.
 
@@ -89,16 +148,15 @@ def min_distance_vector(vector_index, vectors):
     """
 
     min_distance = 10000000
-    min_index = vector_index
+    min_index = 10000000
 
-    for index, target in enumerate(vectors):
+    for index, group in enumerate(groups):
+        for target_vector in group:
+            if euclidian_distance(vector, target_vector) < min_distance:
+                min_distance = euclidian_distance(vector, target_vector)
+                min_group = index
 
-        if euclidian_distance(vectors[vector_index], target) < min_distance:
-            min_distance = euclidian_distance(vectors[vector_index], target)
-            min_index = index
-    print(vectors[min_index], min_distance)
-
-    return vectors[min_index], min_distance
+    return min_group, min_distance
 
 
 def min_max_between_all(vectors):
@@ -115,23 +173,10 @@ def min_max_between_all(vectors):
 
     distances = []
 
-    if file_exists('distances.json'):
-        with open('distances.json') as file:
-
-            for line in file:
-
-                line = line.strip()
-                distances.append(line)
-    else:
-
-        for i in range(0, len(vectors)):
-            for j in range(i + 1, len(vectors)):
-                distance = euclidian_distance(vectors[i], vectors[j])
-                distances.append(distance)
-
-        with open('distances.json', 'w') as file:
-            json.dump(distances, file)
-
+    for i in range(0, len(vectors)):
+        for j in range(i + 1, len(vectors)):
+            distance = euclidian_distance(vectors[i], vectors[j])
+            distances.append(distance)
 
     return min(distances), max(distances)
 
@@ -150,16 +195,16 @@ def BSAS(threshold, q, vectors):
     """
 
     m = 1
-    Cm = [[vectors[0]]] # List of groups
+    Cm = [[vectors[0]]]  # List of groups
 
     vectors_count = len(vectors)
 
     for i in range(1, vectors_count):
-        print("BSAS IIIII : {}".format(i))
-        min_vector, min_distance = min_distance_vector(i, vectors)
 
+        min_group, min_distance = group_with_min_distance(vectors[i], Cm)
 
         if min_distance > threshold and m < q:
+
             # Create a new group
             m = m + 1
             new_group = []
@@ -169,110 +214,49 @@ def BSAS(threshold, q, vectors):
             # Adds the new group to Cm list of groups
             Cm.append(new_group)
         else:
-            min_index = vector_to_group(min_vector, Cm)
-            print(min_index)
-            Cm[min_index].append(vectors[i])
-    
+
+            Cm[min_group].append(vectors[i])
+
     return len(Cm)
 
 
 def get_clusters_count(a, b, c, s, vectors, q):
+    """
+    An implementation of BSAS algorithm. BSAS calculates the number of clusters in a dataset.
+
+    Args:
+        a: This is the list of all vectors.
+        b: maximum number of clusters allowed.
+        c: the vectors generated for the given dataset
+        vectors:
+        q:
+
+    Returns:
+        len(Cm): the count of the found clusters
+    """
 
     groups = []
-    for theta in range(a, b + 1, c):
+    theta = a
+
+    while theta <= b:
+        print("a = {}, b = {}, theta = {}".format(a, b, theta))
         for i in range(0, s):
             shuffle(vectors)
             count = BSAS(theta, q, vectors)
             groups.append(count)
-    #         print("Found {} groups".format(count))
+        theta = theta + c
+    # print("Found {} groups".format(count))
     # print(groups)
     return groups
 
 
-def cleaning_data():
-    """This method prepares the data for processing by BSAS.
-
-        Especially it drops the unnecessary columns, removes the rows of movies 
-        with unknown genre and computes the average rating of each user of each
-        movie genre.
-    """
-
-    # Getting absolute path to the data files
-
-    udata = os.path.abspath('dataset/u.data')
-    uitem = os.path.abspath('dataset/u.item')
-
-    # Creating User Ratings Matrix
-
-    data_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
-
-    ratings = pd.read_csv(udata, sep='\t', names=data_cols,
-                          encoding='latin-1')
-
-    ratings.drop(columns='timestamp', inplace=True, axis=1)
-
-
-    # Creating Movies Matrix
-
-    movie_cols = ['movie_id', 'movie_title', 'release_date', 'video_release date',
-                  'IMDb_URL', 'unknown', 'Action', 'Adventure', 'Animation',
-                  'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
-                  'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
-                  'Thriller', 'War', 'Western']
-
-    movies = pd.read_csv(uitem, sep='|', names=movie_cols,
-                         encoding='latin-1')
-
-
-
-
-    movies = movies[movies.unknown == 0]
-
-    movies.drop(columns=['video_release date', 'release_date',
-                         'IMDb_URL', 'unknown', 'movie_title'], inplace=True, axis=1)
-
-
-    # Merging Ratings and Movies
-
-    final_matrix = pd.merge(ratings, movies, on='movie_id', how='inner')
-        
-    df = final_matrix.replace(0, np.NaN) #all 0 values transform to NaN
-
-    for genre in GENRES:
-        df.loc[(df[genre] == 1), genre] = df['rating']
-
-    
-    grouped=df.groupby('user_id').mean() #group by user id and calculate the avg of non-NaN values
-    
-    grouped.drop(columns=['movie_id','rating'], inplace=True, axis=1) #leave only user id and agv for every genre
-
-    # Replaces nan with 0
-    grouped.fillna(0, inplace=True)
-
-    grouped = list(grouped.values.tolist())
-          
-    with open('data.json', 'w') as file:
-        json.dump(grouped, file)
-
-    return grouped
-
-
 if __name__ == '__main__':
-    
-    if file_exists('data.json'):
-        vectors = []
-        with open('data.json') as file:
-            for line in file: 
-                line = line.strip() 
-                vectors.append(line) 
-    else:
-        vectors = cleaning_data()
 
+
+    vectors = cleaning_data()
 
     a, b = min_max_between_all(vectors)[0], min_max_between_all(vectors)[1]
-    a, b = int(float(a)), int(float(b))
 
-    clusters = get_clusters_count(a, b, 400, 15, vectors, 400)
+    clusters = get_clusters_count(a, b, 5, 20, vectors, 2000)
+
     print(clusters)
-
-
