d2596f042700dfe741eba86e0c1debf57aa7f966
gtheofilo <p16034@students.cs.unipi.gr>
2019-02-12 00:37:55 +0200

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,27 +1,22 @@
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import KMeans
 
-def kmeans(k, data):
+def apply_kmeans(k, data):
+    """Applys KMeans"""
 
     kmeans = KMeans(n_clusters=k, init='random', n_init=10, max_iter=300,
     precompute_distances=True, algorithm='full').fit(data)
-    
+
     return kmeans
 
 
-def hierarchical_clustering(clusters, data):
 
-    hierarchical_clustering = AgglomerativeClustering(n_clusters=clusters, affinity='euclidean', linkage='ward').fit(data)
-    
+def apply_hierarchical_clustering(clusters, data):
+    """Applys Hierarcical Clustering"""
+
+    hierarchical_clustering = AgglomerativeClustering(n_clusters=clusters, affinity='euclidean', linkage='ward').fit_predict(data)  
+
     return hierarchical_clustering
 
 
-if __name__ == "__main__":
-    
-    from question2 import get_data, get_clusters_count
-    vectors = get_data()
-    
-    kmeans(number_of_clusters, vectors)
-    hierarchical_clustering(2, vectors)
 
-    a = input()
