4d0318a5207a76685cb94afd2622f6e6f2f4bf24
gtheofilo <p16034@students.cs.unipi.gr>
2019-02-12 04:10:23 +0200

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -13,21 +13,22 @@ if __name__ == "__main__":
     print("The number of clusters found by the BSAS Algorithm is: {}".format(clusters_count))
 
     # Question 3
-    kmeans = apply_kmeans(clusters_count, vectors)
+    kmeans = apply_kmeans(clusters_count).fit(vectors)
+
 
     print("KMeans Clustering")
 
-    print("The centroids of each clusters are")
+    print("The centroids of each cluster are")
     print(kmeans.cluster_centers_)
 
     print("Each vector is positioned in")
-    print(kmeans.predict(vectors).labels)
+    print(kmeans.predict(vectors))
 
-    hierarchical_clustering = apply_hierarchical_clustering(clusters_count, vectors)
+    hierarchical_clustering = apply_hierarchical_clustering(clusters_count)
     
     print("Hierarchical Clustering")
 
     print("Each vector is positioned in")
-    print(hierarchical_clustering.labels)
+    print(hierarchical_clustering.fit_predict(vectors))
 
     print("Each vector is positioned in")
