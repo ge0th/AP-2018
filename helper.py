6ea3ba986f42d31eb21d175badd18e4848d492ba
gtheofilo <p16034@students.cs.unipi.gr>
2019-02-14 06:09:13 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -13,7 +13,26 @@ GENRES = ['Action', 'Adventure', 'Animation',
           'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
           'Thriller', 'War', 'Western']
 
+def movies_to_data():
 
+
+    uitem = os.path.abspath('dataset/u.item')
+
+    movie_cols = ['movie_id', 'movie_title', 'release_date', 'video_release date',
+                  'IMDb_URL', 'unknown', 'Action', 'Adventure', 'Animation',
+                  'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+                  'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
+                  'Thriller', 'War', 'Western']
+
+    movies = pd.read_csv(uitem, sep='|', names=movie_cols,
+                         encoding='latin-1')
+
+    movies.drop(columns=['movie_id', 'unknown', 'video_release date', 'release_date',
+                         'IMDb_URL', 'unknown', 'movie_title'], inplace=True, axis=1)
+
+    return movies.as_matrix()
+
+    
 def get_data():
     """This method prepares the data for processing by BSAS.
 
@@ -76,3 +95,57 @@ def get_data():
     grouped = scaler.transform(grouped)
 
     return grouped
+
+def get_training_data(file_path='dataset/u.data'):
+
+
+    def has_seen(x):
+        if x['rating'] >= 3:
+            return 1
+        else:
+            return 0
+
+    # Getting absolute path to the data files
+
+    udata = os.path.abspath(file_path)
+    uitem = os.path.abspath('dataset/u.item')
+    # Creating User Ratings Matrix
+
+    data_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+
+    ratings = pd.read_csv(udata, sep='\t', names=data_cols,
+                          encoding='latin-1')
+
+    ratings.drop(columns='timestamp', inplace=True, axis=1)
+
+
+    movie_cols = ['movie_id', 'movie_title', 'release_date', 'video_release date',
+                  'IMDb_URL', 'unknown', 'Action', 'Adventure', 'Animation',
+                  'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+                  'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
+                  'Thriller', 'War', 'Western']
+
+    movies = pd.read_csv(uitem, sep='|', names=movie_cols,
+                         encoding='latin-1')
+
+    movies = movies[movies.unknown == 0]
+
+    movies.drop(columns=['video_release date', 'release_date',
+                         'IMDb_URL', 'unknown', 'movie_title'], inplace=True, axis=1)
+
+    final_matrix = pd.merge(ratings, movies, on='movie_id', how='inner')
+
+    final_matrix['has_seen'] = final_matrix.apply(has_seen, axis=1)
+    
+    
+    Y_data = final_matrix['has_seen']
+    X_data = final_matrix.drop(columns=['movie_id', 'has_seen', 'rating'], axis=1)
+
+    Y_data = Y_data.values.tolist()
+    X_data = X_data.values.tolist()
+
+    scaler = MinMaxScaler().fit(X_data)
+    X_data = scaler.transform(X_data)
+
+    return X_data, Y_data
+
