1fcd040eb218562d1b078bb84aac4293a9807f69
gtheofilo <p16034@students.cs.unipi.gr>
2019-02-24 15:40:36 +0200

    Update question4.py

--- question4.py
+++ question4.py
@@ -3,11 +3,7 @@ from sklearn.neural_network import MLPClassifier
 from helper import get_data
 from helper import get_training_data, movies_to_data
 
-def user_id_scaler(id):
-    return (id - 1) / (1682 - 1)
 
-def movie_id_scaler(id):
-    return (id - 1) / (943 - 1)
 
 def least_squares_classifier(X_data, Y_data):
 
@@ -18,8 +14,8 @@ def least_squares_classifier(X_data, Y_data):
     
 def neural_network_classifier(X_data, Y_data):
     
-    neural_network = MLPClassifier(hidden_layer_sizes=(19,9), activation='logistic', 
-        solver='sgd', alpha=1e-5, learning_rate='adaptive', max_iter=1000, shuffle=True)
+    neural_network = MLPClassifier(hidden_layer_sizes=(4, 4), activation='logistic', 
+        solver='sgd', alpha=1e-5, learning_rate='adaptive', max_iter=1000)
     neural_network.fit(X_data, Y_data)
 
     return neural_network
